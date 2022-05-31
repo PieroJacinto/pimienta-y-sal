eeca9dd6f6a95481732954a634540c2210cea629
Delfina Callegari <delfinacallegari@gmail.com>
2022-05-31 20:52:48 -0300

    rend menu dinamico

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,10 +1,16 @@
+const platos = require("../platos");
+
 module.exports = {
     home: (req,res)=> {
-        res.render ("home");
+        res.render ("home",{
+            "platos": platos
+        });
 
     },
     detalleMenu: (req,res)=> {
         res.render ("detalleMenu");
     },
     
+    
 };
+
---
+++ platos.js
@@ -0,0 +1,30 @@
+const platos = [
+    {
+        nombre: "Carpaccio fresco",
+        descripcion: "Entrada Carpaccio de salmón con cítricos",
+        precio: 65.50, 
+    },
+    {
+        nombre: "Risotto de berenjena",
+        descripcion: "Risotto de berenjena y queso de cabra ",
+        precio: 47, 
+    },
+    {
+        nombre: "Mousse de arroz",
+        descripcion: "Mousse de arroz con leche y aroma de azahar",
+        precio: 27.50, 
+    },
+    {
+        nombre: "Espárragos blancos",
+        descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+        precio: 35.70, 
+    },
+    {
+        nombre: "Pizza",
+        descripcion: "Napo muy rica",
+        precio: 100, 
+    }
+    
+]
+
+module.exports = platos;
